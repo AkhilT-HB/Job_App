0a7ccebbf913c106c76d264d48364692a7af716d
AkhilT-HB <akhilhb02@gmail.com>
2024-07-16 00:09:20 +0530

    Updated review and company classes with getter setters

--- firstJobApp/src/main/java/com/learning/firstJobApp/company/Company.java
+++ firstJobApp/src/main/java/com/learning/firstJobApp/company/Company.java
@@ -30,6 +30,20 @@ public class Company {
 
 	public Company() {
 	}
+	
+	
+
+	public List<Review> getReviews() {
+		return reviews;
+	}
+
+
+
+	public void setReviews(List<Review> reviews) {
+		this.reviews = reviews;
+	}
+
+
 
 	public long getId() {
 		return id;
