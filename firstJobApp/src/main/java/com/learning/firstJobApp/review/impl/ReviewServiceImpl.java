7d45752f581c0f0698c80a16e508653f964af79a
AkhilT-HB <akhilhb02@gmail.com>
2024-07-17 06:44:06 +0530

    Created the controller and service for getting and posting reviews

--- firstJobApp/src/main/java/com/learning/firstJobApp/review/impl/ReviewServiceImpl.java
+++ firstJobApp/src/main/java/com/learning/firstJobApp/review/impl/ReviewServiceImpl.java
@@ -1,10 +1,50 @@
 package com.learning.firstJobApp.review.impl;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
+import com.learning.firstJobApp.company.Company;
+import com.learning.firstJobApp.company.CompanyRepository;
+import com.learning.firstJobApp.company.CompanyService;
+import com.learning.firstJobApp.review.Review;
+import com.learning.firstJobApp.review.ReviewRepository;
 import com.learning.firstJobApp.review.ReviewService;
 
 @Service
 public class ReviewServiceImpl implements ReviewService{
+	
+	private final ReviewRepository reviewRepository;
+	private final CompanyService companyService;
+	
+
+	public ReviewServiceImpl(ReviewRepository reviewRepository, CompanyService companyService) {
+		this.reviewRepository = reviewRepository;
+		this.companyService= companyService;
+	}
+
+
+
+	@Override
+	public List<Review> getAllReviews(Long companyId) {
+		List<Review> reviews = reviewRepository.findByCompanyId(companyId);
+		return reviews;
+	}
+
+
+
+	@Override
+	public boolean addReview(Long companyId, Review review) {
+		Company company = companyService.getCompanyById(companyId);
+		if(company != null) {
+			review.setCompany(company);
+			reviewRepository.save(review);
+			return true;
+		}else {
+			return false;
+		}
+		
+	}
 
 }
